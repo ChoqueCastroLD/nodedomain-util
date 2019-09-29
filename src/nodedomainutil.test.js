09141588ca5581f2f7f0809d137aac4916a3a42e
ChoqueCastroLD <ghoulhunter@outlook.com.p>
2019-09-29 17:50:00 -0500

    even more tests

--- src/nodedomainutil.test.js
+++ src/nodedomainutil.test.js
@@ -2,22 +2,48 @@ const ndt = require('./nodedomainutil.js');
 
 test('getDomain test, passing a localhost url should return localhost domain', () => 
     expect(
-        ndt.getDomain('http://localhost/')
+        ndt.getDomain('http://localhost:90/')
     )
     .toBe(
-        'localhost'
+        'localhost:90'
     )
 )
 
 test('getDomain test, passing a url should return its domain name', () => 
     expect(
-        ndt.getDomain('http://www.luisChoQUe.coM/')
+        ndt.getDomain('http://luisChoQUe/')
+    )
+    .toBe(
+        'luischoque'
+    )
+)
+
+test('getDomain test, passing a url should return its domain name, no protocol', () => 
+    expect(
+        ndt.getDomain('luisChoQUe:80/help')
+    )
+    .toBe(
+        'luischoque:80'
+    )
+)
+
+test('getDomain test, passing a url should return its domain name, no protocol, no routes', () => 
+    expect(
+        ndt.getDomain('www.luisChoQUe.coM')
     )
     .toBe(
         'luischoque.com'
     )
 )
 
+test('getSubDomain test, passing a url should return its subdomain, no protocol', () => 
+    expect(
+        ndt.getSubDomain('www.npmjs.com/package/nodedomain-util')
+    )
+    .toBe(
+        'www'
+    )
+)
 test('getSubDomain test, passing a url should return its subdomain', () => 
     expect(
         ndt.getSubDomain('ws://WWW.luisChoQUe.coM/')
