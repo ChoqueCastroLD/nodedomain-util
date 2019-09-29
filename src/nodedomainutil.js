9c76e620fd07030f6b4af151c8fccc30d8506b6b
ChoqueCastroLD <ghoulhunter@outlook.com.p>
2019-09-29 17:28:14 -0500

    Code cleaned

--- src/nodedomainutil.js
+++ src/nodedomainutil.js
@@ -1,24 +1,23 @@
 module.exports = {
     /** Removes protocol prefix and routes from url */
     cleanUrl( url ){
-        url = url.split('://')[1];
-        url = url.split('/')[0];
+        let temp = url.split('://')[1].split('/')[0];
 
-        return url.toLowerCase();
+        return temp.toLowerCase();
     },
     /** Pass a url, returns a Domain */
     getDomain( url ){
-        url = this.cleanUrl(url);
+        let arr = this.cleanUrl(url).split('.');
 
-        let arr = url.split('.');
         return (arr[arr.length-2]+"."+arr[arr.length-1]).toLowerCase();
     },
     /** Pass a url, returns Subdomain */
     getSubDomain( url ){
-        url = this.cleanUrl(url);
-        
-        let arr = url.split('.');
-        arr.pop(); arr.pop();
+        let arr = this.cleanUrl(url).split('.');
+
+        arr.pop();
+        arr.pop();
+
         return arr.join('.').toLowerCase();
     }
 }
